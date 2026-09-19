8afad180a665f177b8681177cf51a658a2dde9c2
Akshat Pandey <techclashofficial@gmail.com>
2026-09-19 12:13:48 +0530

    Sql Query 2

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,1 +1,105 @@
-select * from cleaned_churn
+select * from cleaned_churn;
+
+# Total Number Of Customers Within The Dataset
+select Count(*) As Total_Customer 
+from cleaned_churn;
+
+
+# Total Number of Customers Who Churned 
+select Count(*) as Churned_customer
+from cleaned_churn
+where `Churn Label` = 'Yes';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
